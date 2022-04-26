706491f2955a562d19a39709f6d3b1b5db38c7da
Maria Lago <50707572+marialago@users.noreply.github.com>
2022-04-26 20:00:34 -0300

    Update ex19.c

--- Lista1/ex19.c
+++ Lista1/ex19.c
@@ -1,1 +1,23 @@
---
+#include <gc/gc.h>
+#include <assert.h>
+#include <stdio.h>
+
+// https://www.hboehm.info/gc/simple_example.html
+// compile with gcc -o result resolution.c -lgc
+
+int main()
+{
+  int i;
+
+  GC_INIT(); /* Optional on Linux/X86; see below.  */
+  for (i = 0; i < 10000000; ++i)
+  {
+    int **p = (int **)GC_MALLOC(sizeof(int *));
+    int *q = (int *)GC_MALLOC_ATOMIC(sizeof(int));
+    assert(*p == 0);
+    *p = (int *)GC_REALLOC(q, 2 * sizeof(int));
+    if (i % 100000 == 0)
+      printf("Heap size = %d\n", GC_get_heap_size());
+  }
+  return 0;
+}
